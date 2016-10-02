0b0f40a04988bb72a13252db96c3f93921f2af03
John Ma <jcema@ust.hk>
2016-10-02 12:36:27 +0800

    added skygear-server

---
+++ skygear-server/src/config/base.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Settings configured here will be merged into the final config object.
+export default {
+  'skygear': {
+    'endPoint': 'https://whatsfordinner.skygeario.com/',
+    'apiKey': '6a99e984aa364443ac9d0ec201d1b7dc'
+  }
+}
+
